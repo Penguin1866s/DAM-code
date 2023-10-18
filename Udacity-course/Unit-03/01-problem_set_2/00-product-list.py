523a77e38677f768d0cce17f711a2f8957e72023
Pengüin1866 <114236318+Penguin1866s@users.noreply.github.com>
2023-10-18 21:45:55 +0200

    problem_set_3 files update

---
+++ Udacity-course/Unit-03/01-problem_set_2/00-product-list.py
@@ -0,0 +1,33 @@
+# Define a procedure, product_list,
+# that takes as input a list of numbers,
+# and returns a number that is
+# the result of multiplying all
+# those numbers together.
+
+def product_list(list):
+    counter = 0
+#    mult_nums_list = 0
+    if len(list) == 0:
+        mult_nums_list = 1
+    else:
+        for j in list:
+            counter += 1
+            if counter > 1:
+                mult_nums_list = mult_nums_list * j
+            else:
+                mult_nums_list = j * 1
+    return mult_nums_list
+
+print(product_list([9]))
+print(product_list([1 , 2 , 3 , 4]))
+print(product_list([]))
+
+#Test:
+#print product_list([9])
+#>>> 9
+
+#print product_list([1,2,3,4])
+#>>> 24
+
+#print product_list([])
+#>>> 1
