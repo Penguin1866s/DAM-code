d478f2fabfac9e346b9f7a321d17f9e1e8e73c8c
Pengüin1866 <114236318+Penguin1866s@users.noreply.github.com>
2023-11-09 13:58:51 +0100

    Add files via upload

---
+++ Rat-book python exercises/03-unified try example.py
@@ -0,0 +1,44 @@
+#This code was not created by me
+#This exercise is to try the exception handling in python(page 1105)
+sep = '-' * 45 + '\n'
+
+print(sep + 'EXCEPTION RAISED AND CAUGHT')
+try:
+    x = 'spam'[99]
+except IndexError:
+    print('except run')
+finally:
+    print('finally run')
+print('after run')
+
+
+print('\n' + sep + 'NO EXCEPTION RAISED')
+try:
+    x = 'spam'[3]
+except IndexError:
+    print('except run')
+finally:
+    print('finally run')
+print('after run')
+
+
+print('\n' + sep + 'NO EXCEPTION RAISED, WITH ELSE')
+try:
+    x = 'spam'[3]
+except IndexError:
+    print('except run')
+else:
+    print('else run')
+finally:
+    print('finally run')
+print('after run')
+
+
+print('\n' + sep + 'EXCEPTION RAISED BUT NOT CAUGHT')
+try:
+    x = 1 / 0
+except IndexError:
+    print('except run')
+finally:
+    print('finally run')
+print('after run')
