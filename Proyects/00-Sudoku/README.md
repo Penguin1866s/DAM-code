2bea837574c8030330f3118d1e97f2d2c4730d66
Pengüin1866 <elobjetivopenguin1866@gmail.com>
2024-04-21 14:19:32 +0200

    fix(Proyects/*): Delete the folder 'Proyects'.

--- Proyects/00-Sudoku/README.md
+++
@@ -1,3 +0,0 @@
-# Check sudoku
-<p>A program to check if a sudoku is correct or not.</p>
-<p>(Some of the sudoku input will be in docstrings.)</p>
--- Proyects/00-Sudoku/modulated/sudoku_tdd_(pytest)/src/s00_check_issquare.py
+++
@@ -1,5 +0,0 @@
-def check_issquare(sudoku):
-    if all(len(row) == len(sudoku) for row in sudoku):
-        return True
-    else:
-        return False
