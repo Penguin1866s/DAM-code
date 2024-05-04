0c50e5a039af35efa370c728078f1a6dcabfed87
Pengüin1866 <elobjetivopenguin1866@gmail.com>
2024-05-05 01:09:25 +0200

    build(Java-book_Try_it_out/UsingNamedGroups.java): Extension of 'UsingNamedGroups.java'

--- Java-book_Try_it_out/try_it_now_java_book/app/src/main/java/edu/badpals/try_it_now_java_book/example_09_RegularExpresions/UsingNamedGroups.java
+++ Java-book_Try_it_out/try_it_now_java_book/app/src/main/java/edu/badpals/try_it_now_java_book/example_09_RegularExpresions/UsingNamedGroups.java
@@ -14,9 +14,26 @@ public class UsingNamedGroups {
         Pattern p = Pattern.compile(regex);
         // Get Matcher object
         Matcher m = p.matcher(source);
+
+
+        // Using the start() and end() methods with the inputs of named groups and numbered groups
+        System.out.println("\n\nUsing the start() and end() methods with the inputs of named groups and numbered groups:");
+        while(m.find()) {
+            String matchedText = m.group();
+            int start1 = m.start("areaCode");
+            int start2 = m.start("prefix");
+            int start3 = m.start("lineNumber");
+            System.out.print("\tMatched Text:" + matchedText);
+            System.out.print("\t. Area code start:" + start1);
+            System.out.print("\t, Prefix start:" + start2);
+            System.out.println("\t, Line Number start:" + start3);
+        }
+
         // Replace the phone numbers by formatted phone numbers
+        System.out.println("\nReplacing the phone numbers by formatted phone numbers:");
+
         String formattedSource = m.replaceAll(replacementText);
-        System.out.println("Text: " + source);
-        System.out.println("Formatted Text: " + formattedSource);
+        System.out.println("\tText: " + source);
+        System.out.println("\tFormatted Text: " + formattedSource);
     }
 }
